0d469082c71cf260273bb3b653312718718d2942
ThanhDuy-173 <75257425+ThanhDuy-173@users.noreply.github.com>
2022-03-12 14:56:39 +0700

    On deploy

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,23 +4,24 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBaO78G3EmNPTuQYrsk_HFkp5AjLwJWCxY",
-//   authDomain: "thankz-825a3.firebaseapp.com",
-//   projectId: "thankz-825a3",
-//   storageBucket: "thankz-825a3.appspot.com",
-//   messagingSenderId: "704108020793",
-//   appId: "1:704108020793:web:e24eb2aca07c41bd0e39e8"
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBcz1AHclS4zP3UMsHNtyht57-z_nCVbXk",
-  authDomain: "thankzweb.firebaseapp.com",
-  projectId: "thankzweb",
-  storageBucket: "thankzweb.appspot.com",
-  messagingSenderId: "409659029538",
-  appId: "1:409659029538:web:a45783236c07d5d428f27b"
+  apiKey: "AIzaSyBaO78G3EmNPTuQYrsk_HFkp5AjLwJWCxY",
+  authDomain: "thankz-825a3.firebaseapp.com",
+  projectId: "thankz-825a3",
+  storageBucket: "thankz-825a3.appspot.com",
+  messagingSenderId: "704108020793",
+  appId: "1:704108020793:web:e24eb2aca07c41bd0e39e8"
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD91ATRR1Y9Rfg08VinTk4saHBk_MyuS3Q",
+//   authDomain: "thankzking.firebaseapp.com",
+//   projectId: "thankzking",
+//   storageBucket: "thankzking.appspot.com",
+//   messagingSenderId: "955444338044",
+//   appId: "1:955444338044:web:db5977257897d94748d8f9",
+//   measurementId: "G-T57TSMQED7"
+// };
 
 firebase.initializeApp(firebaseConfig);
 
