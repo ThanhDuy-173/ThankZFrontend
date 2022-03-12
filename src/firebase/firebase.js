731660ffc310ca761be37e94e0663c32861b647b
ThanhDuy-173 <75257425+ThanhDuy-173@users.noreply.github.com>
2022-03-12 20:44:00 +0700

    update firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,16 +13,6 @@ const firebaseConfig = {
   appId: "1:704108020793:web:e24eb2aca07c41bd0e39e8"
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD91ATRR1Y9Rfg08VinTk4saHBk_MyuS3Q",
-//   authDomain: "thankzking.firebaseapp.com",
-//   projectId: "thankzking",
-//   storageBucket: "thankzking.appspot.com",
-//   messagingSenderId: "955444338044",
-//   appId: "1:955444338044:web:db5977257897d94748d8f9",
-//   measurementId: "G-T57TSMQED7"
-// };
-
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
